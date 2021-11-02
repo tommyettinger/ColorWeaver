f782dd2d14e2cefc6d07fad05e6b451a68321667
Tommy Ettinger <tommy.ettinger@gmail.com>
2021-11-01 18:30:40 -0700

    This shows what not to do for a palette meant for dithering.
    Both sweet-24, by bess, and 24-universal, by Danilo Recevic, allocate several similar colors to a band of extremely similar lightness. If this had more variation instead of a band, then dithering things like the Mona Lisa would be able to scale up lightness more gradually. Instead, it has to roughly jump from light to dark and back again.

--- src/test/java/colorweaver/HexGenerator.java
+++ src/test/java/colorweaver/HexGenerator.java
@@ -15,7 +15,7 @@ import static colorweaver.tools.TrigTools.sin_;
 
 public class HexGenerator extends ApplicationAdapter {
     private int[] palette;
-    public static final String NAME = "cc-29";
+    public static final String NAME = "universal-24";
 
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
