98659d3b4756ec4887814f28dfcb71d63a65f468
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-09-06 16:15:13 -0700

    Generate big triangular blue noise array.

--- src/test/java/colorweaver/BlueNoiseOther.java
+++ src/test/java/colorweaver/BlueNoiseOther.java
@@ -35,7 +35,7 @@ public class BlueNoiseOther extends ApplicationAdapter {
 //            generatePreloadCode(brights, "BlueNoiseOmniTri.txt");
 //        }
 //        Pixmap pix = new Pixmap(Gdx.files.internal("LDR_LLL1_0.png"));
-        for (int idx = 0; idx < 5; idx++) {
+        for (int idx = 0; idx < 64; idx++) {
             Pixmap pix = new Pixmap(Gdx.files.local("blueNoise/blueTri64_"+idx+".png"));
             ByteBuffer buf = pix.getPixels();
             final int len = pix.getWidth() * pix.getHeight();
@@ -44,7 +44,8 @@ public class BlueNoiseOther extends ApplicationAdapter {
                 brights[i] = buf.get(i);
                 brights[i] += -128;
             }
-            generatePreloadCode(brights, "BlueNoise"+idx+".txt");
+            generatePreloadCode(brights, "BlueNoise.txt");
+//            generatePreloadCode(brights, "BlueNoise"+idx+".txt");
         }
         Gdx.app.exit();
     }
@@ -89,7 +90,7 @@ public class BlueNoiseOther extends ApplicationAdapter {
         }
         sb.append("\".getBytes(\"ISO-8859-1\");\n");
 //        sb.append("\".getBytes(StandardCharsets.ISO_8859_1),\n");
-        Gdx.files.local(filename).writeString(sb.toString(), false, "ISO-8859-1");
+        Gdx.files.local(filename).writeString(sb.toString(), true, "ISO-8859-1");
         System.out.println("Wrote code snippet to " + filename);
     }
 
