1d35c0303e8e0d7c4ef3756d08b17d6d67ab478a
Tommy Ettinger <tommy.ettinger@gmail.com>
2022-10-13 19:33:53 -0700

    Just in time for Halloween, a pumpkin palette.
    Also, my cat looking possessed.

--- src/test/java/colorweaver/HexGenerator.java
+++ src/test/java/colorweaver/HexGenerator.java
@@ -15,7 +15,7 @@ import static colorweaver.tools.TrigTools.sin_;
 
 public class HexGenerator extends ApplicationAdapter {
     private int[] palette;
-    public static final String NAME = "munsellish-255";
+    public static final String NAME = "carver-6";
 
     public static void main(String[] arg) {
         Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
