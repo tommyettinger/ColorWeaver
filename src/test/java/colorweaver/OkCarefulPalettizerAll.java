e63eefa83d52486ecf1c33bb635b9fb0f057c8aa
Tommy Ettinger <tommy.ettinger@gmail.com>
2025-05-01 23:04:14 -0700

    Bluesy... could use some tweaking.

--- src/test/java/colorweaver/OkCarefulPalettizerAll.java
+++ src/test/java/colorweaver/OkCarefulPalettizerAll.java
@@ -395,14 +395,18 @@ public class OkCarefulPalettizerAll extends ApplicationAdapter {
 //////        pm.drawPixmap(sam, 0, 0);
 //////        pm = reducer.reduceCoastal(pm);
 //////        a8png8.writePrecisely(Gdx.files.local(subname + "_Coastal" + suffix + ".png"), pm, PALETTE, false, 0);
-////// nice and griddy, for things that can use that
-        pm.drawPixmap(sam, 0, 0);
-        pm = reducer.reducePatternish(pm);
-        a8png8.writePrecisely(Gdx.files.local(subname + "_Patternish" + suffix + ".png"), pm, PALETTE, false, 0);
+//////// nice and griddy, for things that can use that
+//        pm.drawPixmap(sam, 0, 0);
+//        pm = reducer.reducePatternish(pm);
+//        a8png8.writePrecisely(Gdx.files.local(subname + "_Patternish" + suffix + ".png"), pm, PALETTE, false, 0);
+//////// has some weird horizontal artifacts
+//        pm.drawPixmap(sam, 0, 0);
+//        pm = reducer.reduceMicroPatternish(pm);
+//        a8png8.writePrecisely(Gdx.files.local(subname + "_MicroPatternish" + suffix + ".png"), pm, PALETTE, false, 0);
 //////
         pm.drawPixmap(sam, 0, 0);
-        pm = reducer.reduceMicroPatternish(pm);
-        a8png8.writePrecisely(Gdx.files.local(subname + "_MicroPatternish" + suffix + ".png"), pm, PALETTE, false, 0);
+        pm = reducer.reduceBluesy(pm);
+        a8png8.writePrecisely(Gdx.files.local(subname + "_Bluesy" + suffix + ".png"), pm, PALETTE, false, 0);
 ////// Absolutely terrible!
 //        pm.drawPixmap(sam, 0, 0);
 //        pm = reducer.reduceSkitter(pm);
