25cf9c6ac024fc90d0f28c1cb395382d7eaae3f1
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-05-27 20:26:09 -0700

    Clamping per-channel helps Roberts a bit.
    This also uses a different strength calculation, pulled from the other triangle-wave-based dither, Marten.

--- src/test/java/colorweaver/OkCarefulPalettizerOne.java
+++ src/test/java/colorweaver/OkCarefulPalettizerOne.java
@@ -249,8 +249,8 @@ public class OkCarefulPalettizerOne extends ApplicationAdapter {
 //        pm = reducer.reduceReallySimple(pm);
 //        a8png8.writePrecisely(Gdx.files.local(subname + "_ReallySimple" + suffix + ".png"), pm, PALETTE, false, 0);
 
-        pm = reducer.reduceLoaf(pm);
-        a8png8.writePrecisely(Gdx.files.local(subname + "_LoafNew" + suffix + ".png"), pm, PALETTE, false, 0);
+        pm = reducer.reduceRoberts(pm);
+        a8png8.writePrecisely(Gdx.files.local(subname + "_RobertsNew" + suffix + ".png"), pm, PALETTE, false, 0);
 
     }
 }
