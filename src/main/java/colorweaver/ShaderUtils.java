74b459d1af1872422ec20ccbc0af189ffe3cc498
Tommy Ettinger <tommy.ettinger@gmail.com>
2026-06-18 20:58:14 -0700

    Try to bring over Baydient, let's see...

--- src/main/java/colorweaver/ShaderUtils.java
+++ src/main/java/colorweaver/ShaderUtils.java
@@ -187,6 +187,30 @@ public class ShaderUtils {
                     "   gl_FragColor.a = v_color.a * tgt.a;\n" +
                     "}";
 
+    public static final String fragmentShaderBaydient =
+            "varying vec2 v_texCoords;\n" +
+                    "varying vec4 v_color;\n" +
+                    "uniform sampler2D u_texture;\n" +
+                    "uniform sampler2D u_palette;\n" +
+                    "uniform float u_colors;\n" +
+                    "const float b_adj = 31.0 / 32.0;\n" +
+                    "const float rb_adj = 32.0 / 1023.0;\n" +
+                    "const mat4 bayer =(mat4(0.,  8.,  2.,  10., " +
+                    "                        12., 4.,  14., 6.,  " +
+                    "                        3.,  11., 1.,  9.,  " +
+                    "                        15., 7.,  13., 5.) - 7.5) * (0.0625 * 0.45);\n" +
+                    "void main()\n" +
+                    "{\n" +
+                    "   int x = int(mod(gl_FragCoord.x, 4.));\n" +
+                    "   int y = int(mod(gl_FragCoord.y, 4.));\n" +
+                    "   float adj = (bayer[y][x] + ((fract(gl_FragCoord.x * 0.75488 + gl_FragCoord.y * 0.56984) - 0.5)) * 1.5) * u_colors;\n" +
+                    "   vec4 tgt = texture2D( u_texture, v_texCoords );\n" +
+                    "   tgt.rgb = clamp(sqrt(tgt.rgb) + adj, 0.0, 1.0);\n" +
+                    "   tgt.rgb *= tgt.rgb;\n" +
+                    "   vec4 used = texture2D(u_palette, vec2((tgt.b * b_adj + floor(tgt.r * 31.999)) * rb_adj, 1.0 - tgt.g));\n" +
+                    "   gl_FragColor.rgb = v_color.rgb * used.rgb;\n" +
+                    "   gl_FragColor.a = v_color.a * tgt.a;\n" +
+                    "}";
     /**
      * This fragment shader substitutes colors with ones from a palette, acting like {@link #fragmentShader} but also
      * allowing color space adjustments to be done after the palette swap (this won't change the color count). The
